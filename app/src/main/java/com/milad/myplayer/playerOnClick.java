cca3a045b405cec4a3e3aaae94c2b5ee552224ec
milad <targholi2112@gmail.com>
2020-02-13 16:58:35 +0330

    enable dataBinding in player View

---
+++ app/src/main/java/com/milad/myplayer/playerOnClick.java
@@ -0,0 +1,16 @@
+package com.milad.myplayer;
+
+public interface playerOnClick {
+
+    void play();
+
+    void nextSong();
+
+    void previousSong();
+
+    void star();
+
+    void listItem();
+
+    void showBottomSheet();
+}
